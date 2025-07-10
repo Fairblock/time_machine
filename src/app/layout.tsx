fc6c76bf2b6737b6e6c49924dd20c472abb592c2
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2025-07-10 17:18:08 -0400

    debug og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,36 @@
 import ClientLayout from "@/app/ClientLayout";
+import type { Metadata } from "next";
 import "./globals.css";
 import { HowItWorksProvider } from "@/contexts/HowItWorksContext";
 // import Header from '@/components/header/Header';
 // import { useActiveToken } from '@/hooks/useActiveToken';
+
+export const metadata: Metadata = {
+  title:        'Time Machine',
+  description:  'Step into the time machine.',
+  openGraph: {
+    title:       'Time Machine',
+    description: 'Step into the time machine.',
+    url:         'https://timemachine.fairblock.network/',
+    siteName:    'Fairblock',
+    images: [
+      {
+        url:   'https://timemachine.fairblock.network/og.png',
+        width: 1200,
+        height: 630,
+        alt:  'Time Machine',
+      },
+    ],
+    locale: 'en_US',
+    type:   'website',
+  },
+  twitter: {
+    card:        'summary_large_image',
+    title:       'Time Machine',
+    description: 'Step into the time machine.',
+    images:      ['https://timemachine.fairblock.network/og.png'],
+  },
+}
 
 export default function RootLayout({
   children,
