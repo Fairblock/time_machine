269d8d92658a71dab156cfc871718873a090276e
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2025-07-25 10:49:19 -0400

    supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,11 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { FAIRYRING_ENV } from '@/constant/env';
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
-
+/* ── Supabase admin client ──────────────────────────────────────────────── */
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+  FAIRYRING_ENV.supabase!,
+  FAIRYRING_ENV.supabaseKey!
+);
